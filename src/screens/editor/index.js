ada083df3f99c74e3648c6460554743cfcf1892e
Nick_Munene <nickmunene101@gmail.com>
2023-05-06 12:56:28 +0300

    Added image functionality on editor

--- src/screens/editor/index.js
+++ src/screens/editor/index.js
@@ -2,6 +2,9 @@ import React, { useCallback } from "react";
 import {useTheme} from "@react-navigation/native";
 import {SafeAreaView, View, KeyboardAvoidingView} from "react-native";
 import {actions, RichEditor, RichToolbar} from "react-native-pell-rich-editor";
+import ImagePicker from 'react-native-image-picker'
+import PromptAndroid from 'react-native-prompt-android'
+
 
 import FontFamilyStyleSheet from "../../config/utils/stylesheet";
 import NavigationBar from "./NavigationBar";
@@ -64,49 +67,99 @@ const NewNote = React.memo(({navigation, route}) => {
     }
   };
 
+    const handlePressAddImage = () => {
+      const options = {
+        mediaType: 'photo',
+        quality: 1,
+        maxWidth: 800,
+        maxHeight: 800
+      }
+
+      ImagePicker.launchImageLibrary(options, (response) => {
+        if (response.didCancel) {
+          console.log('User cancelled image picker')
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error)
+        } else if (response.uri) {
+          // Handle the selected image uri, e.g., insert it into the editor
+          const imageTag = `<img src="${response.uri}" alt="Image" style="max-width:100%;" />`
+          editorRef.current?.insertHTML(imageTag)
+        }
+      })
+    }
+
+      const handleInsertLink = () => {
+        PromptAndroid(
+          'Insert Link',
+          'Enter the URL:',
+          [
+            {
+              text: 'Cancel',
+              onPress: () => {},
+              style: 'cancel'
+            },
+            {
+              text: 'OK',
+              onPress: (url) => {
+                const linkTag = `<a href="${url}">${url}</a>`
+                editorRef.current?.insertHTML(linkTag)
+              }
+            }
+          ],
+          {
+            type: 'plain-text',
+            cancelable: false
+          }
+        )
+      }
+
+
+    const editorStyle = {
+      initialCSSText: `${FontFamilyStyleSheet}`,
+      contentCSSText: 'font-family: Zilla Slab',
+      backgroundColor: colors.background,
+      color: colors.text
+    }
+
+    const toolbarStyle = {
+      borderTopWidth: 1,
+      borderTopColor: colors.text,
+      backgroundColor: colors.background,
+      color: colors.text
+    }
+
+
   return (
     <SafeAreaView
       style={{
-        flex: 1,
+        flex: 1
       }}
     >
       <NavigationBar onPreviewPress={submitContentHandle} />
       <CustomTextInput />
-      <KeyboardAvoidingView
-        behavior={"height"}
-      >
-        <View style={{marginHorizontal: 10}}>
+      <KeyboardAvoidingView behavior={'height'}>
+        <View style={{ marginHorizontal: 10 }}>
           <RichEditor
             ref={_editor}
             placeholder="Your Note..."
-            androidHardwareAccelerationDisabled={true}
-            initialHeight={"84%"}
-            editorStyle={{
-              initialCSSText: `${FontFamilyStyleSheet}`,
-              contentCSSText: `font-family: Zilla Slab`,
-              backgroundColor: colors.background,
-              color: colors.text,
-            }}
+            androidHardwareAccelerationDisabled
+            initialHeight={'84%'}
+            editorStyle={editorStyle}
             onChange={richTextHandle}
           />
         </View>
         <RichToolbar
           editor={_editor}
-          style={{
-            borderTopWidth: 1,
-            borderTopColor: colors.text,
-            backgroundColor: colors.background,
-            color: colors.text,
-          }}
+          style={toolbarStyle}
           selectedIconTint={colors.text}
           actions={ToolbarTools}
-          onPressAddImage={(_) => console.log(_)}
+          onPressAddImage={handlePressAddImage}
           onInsertLink={(_) => console.log(_)}
           insertVideo={(_) => console.log(_)}
         />
       </KeyboardAvoidingView>
     </SafeAreaView>
-  );
+  )
 });
 
 export default NewNote;
